9bdff3e4ad88163466246c1fc7eb20e42fcdea9e
daniel <danhaoy94@163.com>
2021-02-01 10:27:18 +0800

    ddd

--- src/main/java/com/ydh/offer/Solution1.java
+++ src/main/java/com/ydh/offer/Solution1.java
@@ -1,7 +1,27 @@
 package com.ydh.offer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution1 {
 
+    // 新方法
+    public boolean FindWell(int target, int[][] array) {
+        int row = array.length - 1;
+        int col = 0;
+        while (row >= 0 && col < array[0].length) {
+            if (array[row][col] < target) {
+                col++;
+            } else if (array[row][col] > target) {
+                row--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 从
     public boolean Find(int target, int[][] array) {
 
         if (array == null || array[0] == null || array[0].length == 0) {
@@ -16,7 +36,7 @@ public class Solution1 {
         return false;
     }
 
-
+    // 二分查找
     public boolean findOne(int target, int[] array) {
         int left = 0;
         int right = array.length - 1;
@@ -37,9 +57,9 @@ public class Solution1 {
 
     public static void main(String[] args) {
         int target = 6;
-
         int[] array = {1, 2, 6, 7, 8, 9};
 
         System.out.println(new Solution1().findOne(target, array));
+        Map<Integer, Integer> map = new HashMap<>();
     }
 }
